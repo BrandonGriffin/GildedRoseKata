a8147c06c3667cd5c374134c36a9e0599125b3d0
Brandon Griffin <bgriffin@qualtrax.com>
2014-01-17 11:01:37 -0500

    Passed the first test, and renamed the class.

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using GildedRose.Console;
 using NUnit.Framework;
 
 namespace GildedRose.Tests
@@ -5,10 +7,36 @@ namespace GildedRose.Tests
     [TestFixture]
     public class TestAssemblyTests
     {
-        [Test]
-        public void TestTheTruth()
+        private List<Item> Items;
+        private Updater updater;
+
+        [SetUp]
+        public void SetUp()
         {
-            Assert.IsTrue(true);
+            updater = new Updater();
+
+            Items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 15,
+                        Quality = 20
+                    },
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+        }
+
+        [Test]
+        public void SellInGoesDownByOneEachDay()
+        {
+            updater.UpdateQuality(Items);
+
+            Assert.That(Items[0].SellIn == 9);
         }
     }
 }
